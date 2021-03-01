f48e3dc6b0cd8b957f98c0fab7d4b661c176cb1a
Aaron Rizzuto <aaron.rizzuto23@gmail.com>
2021-03-01 10:56:44 -0600

    Update testscript.py
    Added comment instructions in testscript.py to help with getting it all going.

--- testscript.py
+++ testscript.py
@@ -9,16 +9,18 @@ import pandas as pd
 import image_handling
 import mask_functions
 import bispec
-##data location
-datadir = os.getenv("HOME")+'/data_local/nirc2/ADAM/1204/'
+##data location: Replace with you own file locations
+datadir = os.getenv("HOME")+'/data_local/nirc2/ADAM/1204/' 
+
 
 
 import numpy as np
 import glob,os,sys
 
-##find an example datafile
+## Centroid of all targets file produced by Adam Kraus' preprocessing code. Replace with newest version if applicable
 infofile = 'targlistcentroids.txt'
 
+##find an example datafile and read in the centroid information
 ff = open(infofile,'r')
 tnum = []
 xcen = []
@@ -42,14 +44,14 @@ fprefix=np.array(fprefix)
 #pdb.set_trace()
 
 ##Select a target, this is a random FFTau image
-qwe= 99578-4 ##This happens to be n0388.fits, this first in a set of 8 for FFTau in future this has to be automated
+qwe= 99578-4 ##This happens to be n0388.fits, the first in a set of 8 for FFTau in future this has to be automated but that's up to the users setup
 targetprefix = fprefix[qwe]
 
 
 fftau_index = [x for x in range(qwe,qwe+8)]
 iptau_index = [x for x in range(qwe+8,qwe+8+8)]
 runindex = fftau_index + iptau_index
-reprocess = True
+reprocess = True ##This will turn off the file-by-file part ofthe bispectrum calculation to save some time in testing
 if reprocess == True:
     #pdb.set_trace()
     ##This runs everything bispec related that can be done on a single frame, except compute the closure-phase triple product because 
@@ -65,10 +67,15 @@ if reprocess == True:
     fftau_nrmfiles = outfiles[0:8]
     iptau_nrmfiles = outfiles[8:]
 
+### Now do the group run statistics calculations: Here I've group two examples which are a target-calibrator pair by hand, this should be done in a more automated way but that will depend on setup
 fftau_nrmfiles = ['/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33780.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33807.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33835.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33862.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33890.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33918.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33946.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.33973.LDIFNRM.pkl']
 iptau_nrmfiles = ['/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34094.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34122.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34151.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34179.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34206.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34234.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34261.LDIFNRM.pkl', '/Users/acr2877/data_local/nirc2/ADAM/NRM/2012/1204/N2.20121204.34293.LDIFNRM.pkl']
+
+## This will run the final group biscpetrum calculations for the first of the two groups, and save it in a standard location inside of your data directory according to Adam's naming formats
+## see bispec.pro for options on changing the save location.
 dummy = bispec.bispectrum_runstats(fftau_nrmfiles)
 
+##At this point you would do the calibration, which for closure phase is just subtracting the calibrator closure phase from the targets, and dividiong the visibilities. idlNRM has 
+## fancy features here but they aren't all useful.
 
-pdb.set_trace()
 print("Done")
